b4b7c2c5716629b05d3bf6232185ae2453e8bf1a
Ian Daniher <it.daniher@gmail.com>
2014-03-13 12:02:52 -0400

    add headless flowgraph as default

--- src/temps.rs
+++ src/temps.rs
@@ -5,8 +5,6 @@ extern crate dsputils;
 extern crate kpn;
 extern crate instant;
 extern crate sensors;
-extern crate vidsink2;
-extern crate sdl2;
 
 use std::comm::{Port, Chan};
 use instant::{Parts, Head, Body, Tail, Fork, Leg, Funnel};
@@ -18,7 +16,6 @@ fn pktTempA(a: Port<Token>, b: Chan<Token>, c: SourceConf) {kpn::decoder(a,b,c,~
 fn pktTempB(a: Port<Token>, b: Chan<Token>, c: SourceConf) {kpn::decoder(a,b,c,~[15, 6, 5, 8, 2, 9, 1, 4, 2, 9, 4])}
 
 fn main() {
-	sdl2::init([sdl2::InitVideo]);
 	let conf = SourceConf{Freq: 434e6, Rate: 0.256e6, Period: 5e-4};
 	// flowgraph leg for silver&black temp sensor
 	let t1 = ~[Body(sensors::validTokenA), Body(pkt36), Body(pktTempA), Body(sensors::sensorUnpackerA)];
@@ -26,7 +23,7 @@ fn main() {
 	let t2 = ~[Body(kpn::validTokenManchester), Body(kpn::manchesterd), Body(pkt195), Body(pktTempB), Body(sensors::sensorUnpackerB)];
 	// main flowgraph
 	let parsing: ~[Parts] = ~[Body(bitfount::discretize), Body(kpn::rle), Body(kpn::dle), Fork(kpn::tuplicator), Leg(t1), Leg(t2), Funnel(kpn::twofunnel), Fork(kpn::tuplicator), Tail(kpn::udpTokenSink), Tail(kpn::printSink)];
-	let fs: ~[Parts] = ~[Head(bitfount::rtlSource), Body(bitfount::trigger), Body(bitfount::filter), Fork(kpn::tuplicator), Leg(~[Tail(vidsink2::vidSink)]), Leg(parsing)];
+	let fs: ~[Parts] = ~[Head(bitfount::rtlSource), Body(bitfount::trigger), Body(bitfount::filter), Leg(parsing)];
 	// spawn
 	instant::spinUp(fs, ~[], conf);
 	let (p, c) = Chan::new();
