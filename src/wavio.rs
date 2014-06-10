9ac1fc940becd703fe57bd90fc567adb7b46e132
Ian Daniher <it.daniher@gmail.com>
2014-06-10 15:22:30 -0400

    add mono & complex wavesource functions

--- src/wavio.rs
+++ src/wavio.rs
@@ -1,21 +1,37 @@
 extern crate sndfile;
-extern crate kpn;
+extern crate num;
 
-use std::num;
+use num::complex::Complex;
 use std::comm::Sender;
-use std::io;
 
-pub fn wavSource(u: Sender<Vec<f32>>, sRate: u32) {
-	let mut timer = io::Timer::new().unwrap();
-	let mut sndf = sndfile::SndFile::new("./in.wav", sndfile::Read).unwrap();
+pub fn wavSourceF32(u: Sender<f32>, fname: &str, sRate: u32) {
+	let mut sndf = sndfile::SndFile::new(fname, sndfile::Read).unwrap();
+	let info = sndf.get_sndinfo();
+	assert_eq!(info.samplerate as u32, sRate);
+	assert_eq!(info.channels as u32, 1);
+	let mut x: ~[f32] = ~[0.0,.. 1024];
+	for _ in range(0, (info.frames/2)/1024) {
+		sndf.read_f32(x.as_mut_slice(), 1024);
+		for &z in x.iter() {
+			u.send(z)
+		}
+	}
+	let (c, p) = channel();
+	p.recv();
+	c.send(());
+}
+
+pub fn wavSourceComplexF32(u: Sender<Complex<f32>>, fname: &str, sRate: u32) {
+	let mut sndf = sndfile::SndFile::new(fname, sndfile::Read).unwrap();
 	let info = sndf.get_sndinfo();
 	assert_eq!(info.samplerate as u32, sRate);
 	assert_eq!(info.channels as u32, 2);
 	let mut x: ~[f32] = ~[0.0,.. 1024];
 	for _ in range(0, (info.frames/2)/1024) {
 		sndf.read_f32(x.as_mut_slice(), 1024);
-		u.send(x.chunks(2).map(|z| Flt(z[0].hypot(z[1]))).collect());
-		timer.sleep(100);
+		for z in x.chunks(2) {
+			u.send(Complex{re: z[0], im: z[1]});
+		}
 	}
 	let (c, p) = channel();
 	p.recv();
