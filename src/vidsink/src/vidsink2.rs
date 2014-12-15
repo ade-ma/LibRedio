92395a9c9f18c75b123373563082e1ba8f23d75d
Ian Daniher <it.daniher@gmail.com>
2014-12-15 10:05:17 -0500

    expand enum path

--- src/vidsink/src/vidsink2.rs
+++ src/vidsink/src/vidsink2.rs
@@ -2,12 +2,9 @@
 
 extern crate sdl2;
 extern crate dsputils;
-extern crate native;
 
-use sdl2::{video};
 use std::comm;
 use std::task::TaskBuilder;
-use native::task::NativeTaskBuilder;
 use std::comm::{Receiver, Sender, Select, Handle, channel};
 
 
@@ -21,7 +18,7 @@ pub fn draw_vector_as_barplot (renderer: &sdl2::render::Renderer, mut data: Vec<
 		if ((x as f32*decimate_factor) - (x as f32*decimate_factor).floor()) < decimate_factor { Some(y) } else { None }
 	 ).collect();
 	// black screen background
-	renderer.set_draw_color(sdl2::pixels::RGB(0, 0, 0));
+	renderer.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
 	renderer.clear();
 	// calculate bar width
 	let width: f32 = sw as f32 / (data.len() as f32);
@@ -32,7 +29,7 @@ pub fn draw_vector_as_barplot (renderer: &sdl2::render::Renderer, mut data: Vec<
 	// calculate height scale value
 	let scale: f32 = height / ((dmax-dmin));
 	let width = if width > 1.0 { width } else { 1.0 };
-	renderer.set_draw_color(sdl2::pixels::RGB(0, 127, 7));
+	renderer.set_draw_color(sdl2::pixels::Color::RGB(0, 127, 7));
 	let rs: Vec<sdl2::rect::Rect> = range(0, data.len()).map(|i| {
 		let x = data[i];
 		let mut yf = if dmin > 0.0 { height } else {height*0.5f32};
@@ -49,18 +46,18 @@ pub fn draw_vector_as_barplot (renderer: &sdl2::render::Renderer, mut data: Vec<
 }
 
 pub fn vidsink_vecs(pDataC: comm::Receiver<Vec<f32>>) {
-	let window =  match sdl2::video::Window::new("sdl2 vidsink", sdl2::video::PosCentered, sdl2::video::PosCentered, 1300, 600, sdl2::video::SHOWN) {
+	let window =  match sdl2::video::Window::new("sdl2 vidsink", sdl2::video::WindowPos::PosCentered, sdl2::video::WindowPos::PosCentered, 1300, 600, sdl2::video::SHOWN) {
 		Ok(window) => window,
 		Err(err) => panic!("")
 	};
-	let renderer =  match sdl2::render::Renderer::from_window(window, sdl2::render::DriverAuto, sdl2::render::SOFTWARE){
+	let renderer =  match sdl2::render::Renderer::from_window(window, sdl2::render::RenderDriverIndex::Auto, sdl2::render::SOFTWARE){
 		Ok(renderer) => renderer,
 		Err(err) => panic!("")
 	};
 	renderer.set_logical_size(1300, 600);
 	'main : loop {
 		match sdl2::event::poll_event() {
-			sdl2::event::QuitEvent(_) => break 'main,
+			sdl2::event::Event::Quit(_) => break 'main,
 			_ => {}
 		}
 		match pDataC.try_recv() {
@@ -74,11 +71,11 @@ pub fn vidsink_vecs(pDataC: comm::Receiver<Vec<f32>>) {
 	sdl2::quit();
 }
 pub fn vidsink(pDataC: comm::Receiver<f32>, size: uint) {
-	let window =  match sdl2::video::Window::new("sdl2 vidsink", sdl2::video::PosCentered, sdl2::video::PosCentered, 1300, 600, sdl2::video::SHOWN) {
+	let window =  match sdl2::video::Window::new("sdl2 vidsink", sdl2::video::WindowPos::PosCentered, sdl2::video::WindowPos::PosCentered, 1300, 600, sdl2::video::SHOWN) {
 		Ok(window) => window,
 		Err(err) => panic!("")
 	};
-	let renderer =  match sdl2::render::Renderer::from_window(window, sdl2::render::DriverAuto, sdl2::render::SOFTWARE){
+	let renderer =  match sdl2::render::Renderer::from_window(window, sdl2::render::RenderDriverIndex::Auto, sdl2::render::SOFTWARE){
 		Ok(renderer) => renderer,
 		Err(err) => panic!("")
 	};
@@ -86,7 +83,7 @@ pub fn vidsink(pDataC: comm::Receiver<f32>, size: uint) {
 	renderer.set_logical_size(1300, 600);
 	'main : loop {
 		match sdl2::event::poll_event() {
-			sdl2::event::QuitEvent(_) => break 'main,
+			sdl2::event::Event::Quit(_) => break 'main,
 			_ => {}
 		}
 		data.pop();
@@ -100,19 +97,19 @@ pub fn many_vidsink(u: Vec<comm::Receiver<Vec<f32>>>) {
 	sdl2::init(sdl2::INIT_VIDEO);
 	let l = u.len() as int;
 	for x in u.into_iter() {
-		let window =  match sdl2::video::Window::new("sdl2 vidsink", sdl2::video::PosCentered, sdl2::video::PosCentered, 1300/l, 600, sdl2::video::SHOWN) {
+		let window =  match sdl2::video::Window::new("sdl2 vidsink", sdl2::video::WindowPos::PosCentered, sdl2::video::WindowPos::PosCentered, 1300/l, 600, sdl2::video::SHOWN) {
 			Ok(window) => window,
 			Err(err) => panic!("")
 		};
-		let renderer =  match sdl2::render::Renderer::from_window(window, sdl2::render::DriverAuto, sdl2::render::SOFTWARE){
+		let renderer =  match sdl2::render::Renderer::from_window(window, sdl2::render::RenderDriverIndex::Auto, sdl2::render::SOFTWARE){
 			Ok(renderer) => renderer,
 			Err(err) => panic!("")
 		};
 		renderer.set_logical_size(1300/l, 600);
-		TaskBuilder::new().native().spawn(proc() {
+		TaskBuilder::new().spawn(proc() {
 		'main : loop {
 			match sdl2::event::poll_event() {
-				sdl2::event::QuitEvent(_) => break 'main,
+				sdl2::event::Event::Quit(_) => break 'main,
 				_ => {}
 			};
 			match x.try_recv() {
