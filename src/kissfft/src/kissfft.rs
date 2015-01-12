c11f139675eeade6a656abf13558614e78ccd985
Ian Daniher <it.daniher@gmail.com>
2015-01-11 22:02:34 -0500

    update kissfft for 1.0.0a

--- src/kissfft/src/kissfft.rs
+++ src/kissfft/src/kissfft.rs
@@ -4,7 +4,8 @@ extern crate libc;
 use std::vec;
 use num::complex;
 use std::ptr;
-use std::comm;
+
+use std::sync::mpsc::{Receiver, Sender, channel, Handle, Select};
 
 #[link(name= "kissfft")]
 extern "C" {
@@ -13,16 +14,16 @@ extern "C" {
 	fn kiss_fft_cleanup();
 }
 
-pub fn fft(pin: comm::Receiver<Vec<complex::Complex<f32>>>, cout: comm::Sender<Vec<complex::Complex<f32>>>, block_size: u32, inv: u32) {
+pub fn fft(pin: Receiver<Vec<complex::Complex<f32>>>, cout: Sender<Vec<complex::Complex<f32>>>, block_size: u32, inv: u32) {
 	let kiss_fft_cfg = unsafe {kiss_fft_alloc(block_size, inv, ptr::null_mut(), ptr::null_mut())};
 	loop {
-		let mut fout: Vec<complex::Complex<f32>> = vec::Vec::with_capacity(block_size as uint);
-		unsafe {fout.set_len(block_size as uint)}
-		let din = pin.recv();
-		assert!(din.len() == block_size as uint);
+		let mut fout: Vec<complex::Complex<f32>> = vec::Vec::with_capacity(block_size as usize);
+		unsafe {fout.set_len(block_size as usize)}
+		let din = pin.recv().unwrap();
+		assert!(din.len() == block_size as usize);
 		unsafe {
 			kiss_fft(kiss_fft_cfg, din.as_ptr(), fout.as_mut_ptr());
-			cout.send(fout);
+			cout.send(fout).unwrap();
 		}
 	}
 	unsafe { kiss_fft_cleanup(); }
