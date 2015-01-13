04527333f1342eaf17f60ea4d4b49b4435cbbb7a
Ian Daniher <it.daniher@gmail.com>
2015-01-12 23:26:35 -0500

    update and refactor librtlsdr

--- src/rtlsdr/src/rtlsdr.rs
+++ src/rtlsdr/src/rtlsdr.rs
@@ -1,44 +1,53 @@
 /* Copyright Ian Daniher, 2013, 2014.
    Distributed under the terms of the GPLv3. */
 
+#![allow(unstable)]
 extern crate num;
 extern crate libc;
 
 use num::complex;
 use libc::{c_int, c_uint, c_void};
 use std::sync::mpsc::{Receiver, Sender, channel, Handle, Select};
-use std::ptr;
 use std::vec;
 use std::str;
 use std::ffi;
-use std::mem;
 use std::string;
 use std::thread::Thread;
+use std::cell::UnsafeCell;
+use std::sync::Arc;
+use std::intrinsics;
 
 #[link(name= "rtlsdr")]
 
+#[repr(C)]
+struct RTLSDR_Internal;
+
 #[derive(Clone)]
-pub struct RTLSDR_Dev;
+pub struct RTLSDR_Dev {
+    bx: Arc<UnsafeCell<RTLSDR_Internal>>
+}
+
+unsafe impl Sync for UnsafeCell<RTLSDR_Internal> {}
 
 extern "C" {
-	fn rtlsdr_open(dev: &RTLSDR_Dev, dev_index: u32) -> u32;
+	fn rtlsdr_open(dev: &RTLSDR_Internal, dev_index: u32) -> u32;
 	fn rtlsdr_get_device_count() -> u32;
 	fn rtlsdr_get_device_name(dev_index: u32) -> *const i8;
-	fn rtlsdr_reset_buffer(dev: &RTLSDR_Dev) -> c_int;
-	fn rtlsdr_set_center_freq(dev: &RTLSDR_Dev, freq: u32) -> c_int;
-	fn rtlsdr_set_tuner_gain(dev: &RTLSDR_Dev, gain: u32) -> c_int;
-	fn rtlsdr_set_tuner_gain_mode(dev: &RTLSDR_Dev, mode: u32) -> c_int;
-	fn rtlsdr_read_sync(dev: &RTLSDR_Dev, buf: *mut u8, len: u32, n_read: *mut c_int) -> c_int;
-	fn rtlsdr_read_async(dev: &RTLSDR_Dev, cb: extern "C" fn(*const u8, u32, &Sender<Vec<u8>>), chan: &Sender<Vec<u8>>, buf_num: u32, buf_len: u32) -> c_int;
-	fn rtlsdr_cancel_async(dev: &RTLSDR_Dev) -> c_int;
-	fn rtlsdr_set_sample_rate(dev: &RTLSDR_Dev, sps: u32) -> c_int;
-	fn rtlsdr_get_sample_rate(dev: &RTLSDR_Dev) -> u32;
-	fn rtlsdr_close(dev: &RTLSDR_Dev) -> c_int;
+	fn rtlsdr_reset_buffer(dev: &RTLSDR_Internal) -> c_int;
+	fn rtlsdr_set_center_freq(dev: &RTLSDR_Internal, freq: u32) -> c_int;
+	fn rtlsdr_set_tuner_gain(dev: &RTLSDR_Internal, gain: u32) -> c_int;
+	fn rtlsdr_set_tuner_gain_mode(dev: &RTLSDR_Internal, mode: u32) -> c_int;
+	fn rtlsdr_read_sync(dev: &RTLSDR_Internal, buf: *mut u8, len: u32, n_read: *mut c_int) -> c_int;
+	fn rtlsdr_read_async(dev: &RTLSDR_Internal, cb: extern "C" fn(*const u8, u32, &Sender<Vec<u8>>), chan: &Sender<Vec<u8>>, buf_num: u32, buf_len: u32) -> c_int;
+	fn rtlsdr_cancel_async(dev: &RTLSDR_Internal) -> c_int;
+	fn rtlsdr_set_sample_rate(dev: &RTLSDR_Internal, sps: u32) -> c_int;
+	fn rtlsdr_get_sample_rate(dev: &RTLSDR_Internal) -> u32;
+	fn rtlsdr_close(dev: &RTLSDR_Internal) -> c_int;
 }
 
 
 impl Drop for RTLSDR_Dev {
-    pub fn drop (&mut self) {
+    fn drop (&mut self) {
         self.close();
     }
 }
@@ -46,22 +55,23 @@ impl Drop for RTLSDR_Dev {
 impl RTLSDR_Dev {
 	pub fn close(&self) {
 		unsafe {
-			let success = rtlsdr_close(self);
+			let success = rtlsdr_close(&(*self.bx.get()));
 			assert_eq!(success, 0);
 		}
 	}
 	pub fn set_sample_rate(&self, sps: u32) {
 		unsafe {
-			let success = rtlsdr_set_sample_rate(self, sps);
+			let success = rtlsdr_set_sample_rate(&(*self.bx.get()), sps);
 			assert_eq!(success, 0);
-			println!("actual sample rate: {}", rtlsdr_get_sample_rate(self));
+			println!("actual sample rate: {}", rtlsdr_get_sample_rate(&(*self.bx.get())));
 		}
 	}
-	pub fn open(&self) {
+	pub fn open() -> RTLSDR_Dev {
 		unsafe {
 			let mut i: u32 = 0;
+            let internal: RTLSDR_Internal = intrinsics::init();
 			'tryDevices: loop {
-				let success = rtlsdr_open(self, i);
+				let success = rtlsdr_open(&internal, i);
 				if success == 0 {
 					break 'tryDevices
 				}
@@ -70,46 +80,48 @@ impl RTLSDR_Dev {
 				}
 				i += 1;
 			}
-		}
+       return RTLSDR_Dev {bx: Arc::new(UnsafeCell::new(internal))};
+	   }
 	}
 	pub fn clear_buffer(&self) {
 		unsafe {
-			let success = rtlsdr_reset_buffer(self);
+			let success = rtlsdr_reset_buffer(&(*self.bx.get()));
 			assert_eq!(success, 0);
 		}
 	}
 	pub fn set_frequency(&self, freq: u32) {
 		unsafe {
-			let success = rtlsdr_set_center_freq(self, freq);
+			let success = rtlsdr_set_center_freq(&(*self.bx.get()), freq);
 			assert_eq!(success, 0);
 		}
 	}
 	pub fn set_gain(&self, v: u32) {
 		unsafe {
-			let success = rtlsdr_set_tuner_gain_mode(self, 1);
+			let success = rtlsdr_set_tuner_gain_mode(&(*self.bx.get()), 1);
 			assert_eq!(success, 0);
-			let success = rtlsdr_set_tuner_gain(self, v);
+			let success = rtlsdr_set_tuner_gain(&(*self.bx.get()), v);
 			assert_eq!(success, 0);
 		}
 	}
 	pub fn set_gain_auto(&self) {
 		unsafe {
-			let success = rtlsdr_set_tuner_gain_mode(self, 0);
+			let success = rtlsdr_set_tuner_gain_mode(&(*self.bx.get()), 0);
 			assert_eq!(success, 0);
 		}
 	}
 	pub fn read_async(&self, block_size: u32) -> Receiver<Vec<u8>> {
 		let (chan, port) = channel();
+        let bx = self.bx.clone();
 		Thread::spawn(move || {
 			unsafe{
-				rtlsdr_read_async(self, rtlsdr_callback, &chan, 32, block_size*2);
+				rtlsdr_read_async(&(*bx.get()), rtlsdr_callback, &chan, 32, block_size*2);
 			}
 		});
 		return port;
 	}
 	pub fn stop_async(&self) {
 		unsafe {
-			let success = rtlsdr_cancel_async(self);
+			let success = rtlsdr_cancel_async(&(*self.bx.get()));
 			assert_eq!(success, 0);
 		}
 	}
@@ -117,7 +129,7 @@ impl RTLSDR_Dev {
 		unsafe {
 			let mut n_read: c_int = 0;
 			let mut buffer = vec::Vec::with_capacity(512);
-			let success = rtlsdr_read_sync(self, buffer.as_mut_ptr(), ct, &mut n_read);
+			let success = rtlsdr_read_sync(&(*self.bx.get()), buffer.as_mut_ptr(), ct, &mut n_read);
 			assert_eq!(success, 0);
 			assert_eq!(ct as i32, n_read);
 			return buffer;
