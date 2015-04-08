4f8df8ecfac67b55889b47dea62ecd03cb52670d
Ian Daniher <it.daniher@gmail.com>
2015-04-08 19:03:47 -0400

    rtlsdr -> 1.0b

--- src/rtlsdr/src/rtlsdr.rs
+++ src/rtlsdr/src/rtlsdr.rs
@@ -1,7 +1,9 @@
 /* Copyright Ian Daniher, 2013, 2014.
    Distributed under the terms of the GPLv3. */
+#![feature(libc)]
+#![feature(collections)]
+#![feature(core)]
 
-#![allow(unstable)]
 extern crate num;
 extern crate libc;
 
@@ -12,8 +14,7 @@ use std::vec;
 use std::str;
 use std::ffi;
 use std::string;
-use std::thread::Thread;
-use std::cell::UnsafeCell;
+use std::thread;
 use std::sync::Arc;
 use std::intrinsics;
 
@@ -24,10 +25,10 @@ struct RTLSDR_Internal;
 
 #[derive(Clone)]
 pub struct RTLSDR_Dev {
-    bx: Arc<UnsafeCell<RTLSDR_Internal>>
+    bx: Arc<RTLSDR_Internal>
 }
 
-unsafe impl Sync for UnsafeCell<RTLSDR_Internal> {}
+//unsafe impl Sync for UnsafeCell<RTLSDR_Internal> {}
 
 extern "C" {
 	fn rtlsdr_open(dev: &RTLSDR_Internal, dev_index: u32) -> u32;
@@ -55,15 +56,15 @@ impl Drop for RTLSDR_Dev {
 impl RTLSDR_Dev {
 	pub fn close(&self) {
 		unsafe {
-			let success = rtlsdr_close(&(*self.bx.get()));
+			let success = rtlsdr_close(&(*self.bx));
 			assert_eq!(success, 0);
 		}
 	}
 	pub fn set_sample_rate(&self, sps: u32) {
 		unsafe {
-			let success = rtlsdr_set_sample_rate(&(*self.bx.get()), sps);
+			let success = rtlsdr_set_sample_rate(&(*self.bx), sps);
 			assert_eq!(success, 0);
-			println!("actual sample rate: {}", rtlsdr_get_sample_rate(&(*self.bx.get())));
+			println!("actual sample rate: {}", rtlsdr_get_sample_rate(&(*self.bx)));
 		}
 	}
 	pub fn open() -> RTLSDR_Dev {
@@ -80,48 +81,48 @@ impl RTLSDR_Dev {
 				}
 				i += 1;
 			}
-       return RTLSDR_Dev {bx: Arc::new(UnsafeCell::new(internal))};
+       return RTLSDR_Dev {bx: Arc::new(internal)};
 	   }
 	}
 	pub fn clear_buffer(&self) {
 		unsafe {
-			let success = rtlsdr_reset_buffer(&(*self.bx.get()));
+			let success = rtlsdr_reset_buffer(&(*self.bx));
 			assert_eq!(success, 0);
 		}
 	}
 	pub fn set_frequency(&self, freq: u32) {
 		unsafe {
-			let success = rtlsdr_set_center_freq(&(*self.bx.get()), freq);
+			let success = rtlsdr_set_center_freq(&(*self.bx), freq);
 			assert_eq!(success, 0);
 		}
 	}
 	pub fn set_gain(&self, v: u32) {
 		unsafe {
-			let success = rtlsdr_set_tuner_gain_mode(&(*self.bx.get()), 1);
+			let success = rtlsdr_set_tuner_gain_mode(&(*self.bx), 1);
 			assert_eq!(success, 0);
-			let success = rtlsdr_set_tuner_gain(&(*self.bx.get()), v);
+			let success = rtlsdr_set_tuner_gain(&(*self.bx), v);
 			assert_eq!(success, 0);
 		}
 	}
 	pub fn set_gain_auto(&self) {
 		unsafe {
-			let success = rtlsdr_set_tuner_gain_mode(&(*self.bx.get()), 0);
+			let success = rtlsdr_set_tuner_gain_mode(&(*self.bx), 0);
 			assert_eq!(success, 0);
 		}
 	}
 	pub fn read_async(&self, block_size: u32) -> Receiver<Vec<u8>> {
 		let (chan, port) = channel();
         let bx = self.bx.clone();
-		Thread::spawn(move || {
+		thread::spawn(move || {
 			unsafe{
-				rtlsdr_read_async(&(*bx.get()), rtlsdr_callback, &chan, 32, block_size*2);
+				rtlsdr_read_async(&(*bx), rtlsdr_callback, &chan, 32, block_size*2);
 			}
 		});
 		return port;
 	}
 	pub fn stop_async(&self) {
 		unsafe {
-			let success = rtlsdr_cancel_async(&(*self.bx.get()));
+			let success = rtlsdr_cancel_async(&(*self.bx));
 			assert_eq!(success, 0);
 		}
 	}
@@ -129,7 +130,7 @@ impl RTLSDR_Dev {
 		unsafe {
 			let mut n_read: c_int = 0;
 			let mut buffer = vec::Vec::with_capacity(512);
-			let success = rtlsdr_read_sync(&(*self.bx.get()), buffer.as_mut_ptr(), ct, &mut n_read);
+			let success = rtlsdr_read_sync(&(*self.bx), buffer.as_mut_ptr(), ct, &mut n_read);
 			assert_eq!(success, 0);
 			assert_eq!(ct as i32, n_read);
 			return buffer;
@@ -154,10 +155,10 @@ pub fn get_device_count() -> u32 {
 pub fn get_device_name(dev_index: u32) -> string::String {
 	unsafe {
 		let device_string: *const i8 = rtlsdr_get_device_name(dev_index);
-		return string::String::from_str(str::from_utf8(ffi::c_str_to_bytes(&device_string)).unwrap());
+		return string::String::from_str(str::from_utf8(ffi::CStr::from_ptr(device_string).to_bytes()).unwrap());
 	}
 }
 fn i2f(i: u8) -> f32 {i as f32/127.0 - 1.0}
 pub fn data_to_samples(data: Vec<u8>) -> Vec<complex::Complex<f32>> {
-	data.slice_from(0).chunks(2).map(|i| complex::Complex{re:i2f(i[0]), im:i2f(i[1])}).collect()
+	data[0..].chunks(2).map(|i| complex::Complex{re:i2f(i[0]), im:i2f(i[1])}).collect()
 }
