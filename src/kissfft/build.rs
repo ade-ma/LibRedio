bf91562dc26f79d7ca8344b20fc5d48715ac882e
Ian Daniher <it.daniher@gmail.com>
2015-04-09 21:34:10 -0400

    bump kissfft build system

--- src/kissfft/build.rs
+++ src/kissfft/build.rs
@@ -2,8 +2,7 @@ use std::process::Command;
 use std::env;
 
 fn main() {
-	Command::new("make").args(&["-C", "libkissfft"]).status().unwrap();
-	println!("cargo:rustc-link-search=native={}{}", env::var("PWD").unwrap(), "/libkissfft/");
-	println!("cargo:rustc-link-lib=static=kissfft");
+	Command::new("make").args(&["-C", "libkissfft", "install"]).status().unwrap();
+	println!("cargo:rustc-link-lib=dylib=kissfft");
 }
 
